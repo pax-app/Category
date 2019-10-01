e4952132a39b068907f376323b1d54747c01462b
Lucas Dutra <ldutra98@gmail.com>
2019-10-01 17:57:45 -0300

    pax-app/Wiki#103 Update configuration of multiple stages

--- manage.py
+++ manage.py
@@ -7,11 +7,5 @@ from flask import current_app
 app = create_app()
 cli = FlaskGroup(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://f15jto2ojbvbb18m:ojjbjm6lswedzs91@bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j3uyakutbdjh3tdy'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-
-db.init_app(app)
-
 if __name__ == '__main__':
     cli()
