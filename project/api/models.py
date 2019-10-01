40c99513df3291f998a70e9938174015642c10d0
Lucas Dutra <ldutra98@gmail.com>
2019-10-01 17:58:09 -0300

    pax-app/Wiki#103 Create Provider_Category model boiler plate

--- project/api/models.py
+++ project/api/models.py
@@ -2,5 +2,10 @@ from database import db
 from project import create_app
 from Flask import current_app
 
-class General_Category(db.Models):
+
+class General_Category(db.Model):
     __tablename__ = 'GENERAL_CATEGORY'
+
+
+class Provider_Category(db.Model):
+    __tablename__ = 'PROVIDER_CATEGORY'
