fa46e2412c71e64eb8ed6f98903356b8ce7138c7
FabianaRibas <fabianalv.ribas@gmail.com>
2019-10-02 12:16:17 -0300

    Adding blueprint route to views.py
    Signed-off-by: FabianaRibas <fabianalv.ribas@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -1,11 +1,18 @@
+from project.api.models import GeneralCategory
+from project.api.models import ProviderCategory
 from flask import request, jsonify, Blueprint
 
 category_blueprint = Blueprint('category', __name__)
 
 
-@category_blueprint.route('/category/ping', methods=['GET'])
-def ping_pong():
+@category_blueprint.route('/category/all', methods=['GET'])
+def get_categories():
     return jsonify({
         'status': 'success',
-        'message': 'pong!'
+        'general': {
+            'categories': [PROVIDER_CATEGORY.to_json() for PROVIDER_CATEGORY in ProviderCategory.query.all()]
+        },
+        'provider': {
+            'categories': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
+        }
     })
