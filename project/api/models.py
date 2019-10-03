3a3310e4a383af5b83c99624561b661ff1df33d1
FabianaRibas <fabianalv.ribas@gmail.com>
2019-10-03 01:06:49 -0300

    pax-app/Wiki#148 Adding general category to models.py

--- project/api/models.py
+++ project/api/models.py
@@ -7,32 +7,13 @@ class GeneralCategory(db.Model):
 
     general_category_id = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
-    general_category_name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
 
     def __init__(self, name):
-        self.general_category_name = name
+        self.name = name
 
     def to_json(self):
         return {
-            'general_category_id': self.general_category_id,
-            'general_category_name': self.name,
-        }
-
-
-class ProviderCategory(db.Model):
-    __tablename__ = 'PROVIDER_CATEGORY'
-
-    provider_category_id = db.Column(
-        db.Integer, primary_key=True, autoincrement=True)
-    provider_category_name = db.Column(db.String(50), nullable=False)
-    general_category_id = db.Column(
-        db.Integer, db.ForeignKey('general_category.id'))
-
-    def __init__(self, name):
-        self.provider_category_name = name
-
-    def to_json(self):
-        return {
-            'provider_category_id': self.provider_category_id,
-            'provider_category_name': self.name,
+            'general_category.id': self.general_category_id,
+            'general_category.name': self.name,
         }
