6c9cc082dace4c80199786de87d3b204aae209c8
FabianaRibas <fabianalv.ribas@gmail.com>
2019-10-03 01:11:08 -0300

    pax-app/Wiki#148 Adding route from provider category blueprint to views.py

--- project/api/views.py
+++ project/api/views.py
@@ -14,3 +14,13 @@ def get_general_categories():
             'category': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
         }
     })
+
+
+@provider_category_blueprint.route('/provider/category', methods=['GET'])
+def get_provider_categories():
+    return jsonify({
+        'status': 'success',
+        'provider': {
+            'category': [PROVIDER_CATEGORY.to_json() for PROVIDER_CATEGORY in ProviderCategory.query.all()]
+        }
+    })
