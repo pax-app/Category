b6df18158623278ca884c3c262f92f7cae45ceac
FabianaRibas <fabianalv.ribas@gmail.com>
2019-10-03 01:10:07 -0300

    pax-app/Wiki#148 Adding route to general category blueprint to views.py

--- project/api/views.py
+++ project/api/views.py
@@ -6,14 +6,11 @@ general_category_blueprint = Blueprint('general', __name__)
 provider_category_blueprint = Blueprint('provider', __name__)
 
 
-@category_blueprint.route('/category/all', methods=['GET'])
-def get_categories():
+@general_category_blueprint.route('/general/category', methods=['GET'])
+def get_general_categories():
     return jsonify({
         'status': 'success',
         'general': {
-            'categories': [PROVIDER_CATEGORY.to_json() for PROVIDER_CATEGORY in ProviderCategory.query.all()]
-        },
-        'provider': {
-            'categories': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
+            'category': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
         }
     })
