acc2ba86a701457921e9a74de9b52daa2a89fb90
Lucas Dutra <ldutra98@gmail.com>
2019-10-09 00:16:45 -0300

    pax-app/Wiki#148 Set standard GET request pattern

--- project/api/views.py
+++ project/api/views.py
@@ -10,8 +10,8 @@ category_blueprint = Blueprint('category', __name__)
 def get_general_categories():
     return jsonify({
         'status': 'success',
-        'general': {
-            'category': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
+        'data': {
+            'categories': [GENERAL_CATEGORY.to_json() for GENERAL_CATEGORY in GeneralCategory.query.all()]
         }
     })
 
@@ -20,8 +20,8 @@ def get_general_categories():
 def get_provider_categories():
     return jsonify({
         'status': 'success',
-        'provider': {
-            'category': [PROVIDER_CATEGORY.to_json() for PROVIDER_CATEGORY in ProviderCategory.query.all()]
+        'data': {
+            'categories': [PROVIDER_CATEGORY.to_json() for PROVIDER_CATEGORY in ProviderCategory.query.all()]
         }
     })
 
