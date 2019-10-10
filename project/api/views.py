2be040fe2c049f1b5e9479f4a9bd97612f76816a
Lucas Dutra <ldutra98@gmail.com>
2019-10-10 12:05:07 -0300

    pax-app/Wiki#148 Remove category-provider creation method from category service

--- project/api/views.py
+++ project/api/views.py
@@ -44,39 +44,3 @@ def get_specific_provider_categories(general_category_id):
         }
     }
     return jsonify(response), 200
-
-
-@category_blueprint.route('/categoryprovider', methods=['POST'])
-def set_category_provider_relationship():
-    request_data = request.get_json()
-
-    error_response = {
-        'status': 'fail',
-        'message': 'Wrong JSON'
-    }
-
-    if not request_data:
-        return jsonify(error_response), 400
-
-    works = request_data.get('works')
-
-    provider_id = works.get('provider_id')
-    provider_category_id = works.get('provider_category_id')
-
-    try:
-        works = Works(provider_id=provider_id, provider_category_id=provider_category_id)
-
-        db.session.add(works)
-        db.session.flush()
-        db.session.commit()
-
-        response = {
-            'status': 'success',
-            'data': {
-                'message': 'Relationship was stablished'
-            }
-        }
-        return jsonify(response), 200
-    except exc.IntegrityError:
-        db.session.rollback()
-        return jsonify(error_response), 400
