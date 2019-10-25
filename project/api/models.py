43a87bfd41fbc2c769aa2c72e369450e1c31b081
Felipe Campos <fepas.unb@gmail.com>
2019-10-24 21:59:46 -0300

    pax-app/Wiki#148 adding strategy design pattern to category service
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- project/api/models.py
+++ project/api/models.py
@@ -1,25 +1,67 @@
 from database import db
 from flask import current_app
 
+from flask_sqlalchemy import SQLAlchemy
 
-class GeneralCategory(db.Model):
+
+class Category():
+
+    def to_json(self):
+        pass
+
+
+class Context():
+    """
+    The Context defines the interface of interest to clients.
+    """
+
+    def __init__(self, category: Category):
+        """
+        Usually, the Context accepts a category through the constructor, but
+        also provides a setter to change it at runtime.
+        """
+
+        self._category = category
+
+    @property
+    def category(self):
+        """
+        The Context maintains a reference to one of the Category objects. The
+        Context does not know the concrete class of a category. It should work
+        with all strategies via the Category interface.
+        """
+
+        return self._category
+
+    @category.setter
+    def category(self, category: Category):
+        """
+        Usually, the Context allows replacing a Category object at runtime.
+        """
+
+        self._category = category
+
+    def return_json(self, category):
+        return self._category.to_json(category)
+
+
+class GeneralCategory(db.Model, Category):
+
     __tablename__ = 'GENERAL_CATEGORY'
 
     general_category_id = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, name):
-        self.name = name
+    def to_json(self, category):
 
-    def to_json(self):
         return {
-            'id': self.general_category_id,
-            'name': self.name,
+            'id': category.general_category_id,
+            'name': category.name,
         }
 
 
-class ProviderCategory(db.Model):
+class ProviderCategory(db.Model, Category):
     __tablename__ = 'PROVIDER_CATEGORY'
 
     provider_category_id = db.Column(
@@ -28,12 +70,10 @@ class ProviderCategory(db.Model):
     general_category_id = db.Column(
         db.Integer, db.ForeignKey('general_category.id'))
 
-    def __init__(self, name):
-        self.name = name
+    def to_json(self, category):
 
-    def to_json(self):
         return {
-            'generalId': self.general_category_id,
-            'id': self.provider_category_id,
-            'name': self.name,
+            'generalId': category.general_category_id,
+            'id': category.provider_category_id,
+            'name': category.name,
         }
